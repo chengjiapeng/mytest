3a7bbfc6220d61071dd9b11f71c89235d96df899
chengjiapeng <2695061956@qq.com>
2017-03-30 00:21:43 -0700

    test for if delete is useful

--- myproject/web/src/main/java/com/example/web/controller/AAAController.java
+++
@@ -1,7 +0,0 @@
-package com.example.web.controller;
-
-/**
- * Created by chengjiapeng on 2017/3/30.
- */
-public class AAAController {
-}
--- myproject/web/src/main/java/com/example/web/controller/TestController3.java
+++
@@ -1,8 +0,0 @@
-package com.example.web.controller;
-
-/**
- * Created by chengjiapeng on 2017/3/30.
- */
-public class TestController3 {
-    //第三次修改
-}
